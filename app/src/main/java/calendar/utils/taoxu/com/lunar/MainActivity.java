55466338d86583de5fea474ad2259e4286f9a412
Tao.Xu <240219831@qq.com>
2017-10-09 11:40:17 +0800

    Initial commit

---
+++ app/src/main/java/calendar/utils/taoxu/com/lunar/MainActivity.java
@@ -0,0 +1,19 @@
+package calendar.utils.taoxu.com.lunar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.taoxu.library.utils.calendar.Lunar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv1 = (TextView) findViewById(R.id.tv1);
+        Lunar lunar = Lunar.getInstance();
+        tv1.setText(lunar.getShowStr(2017, 10, 4));
+    }
+}
